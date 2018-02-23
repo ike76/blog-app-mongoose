e7382596979c7a9112754d274b6b33f5ae23dcba
ike76 <ike76@me.com>
2018-02-23 17:09:57 -0600

    errors working no edge cases

--- test/test-blog.js
+++ test/test-blog.js
@@ -48,18 +48,132 @@ describe('BlogPosts resource', function(){
 		return closeServer();
 	});
 
+	const expectedKeys = ['author', 'title', 'content', 'created', 'id'];
 	describe('GET endpoint', function(){
-		it('should return all posts w correct fields', function(){});
-		it('should return a single post with correct fields', function(){});	
+		it('should return all posts w correct fields', function(){
+			let res;
+			return chai.request(app)
+				.get('/posts')
+				.then(function(_res){
+					res = _res;
+					expect(res).to.have.status(200);
+					expect(res).to.be.json;
+					expect(res.body).to.have.length.of.at.least(1)
+					return BlogPost.count();
+				})
+				.then(function(count){
+					expect(res.body.length).to.equal(count)
+					res.body.forEach(post=>{
+						expect(post).to.include.keys(expectedKeys)
+					})
+				})
+		});
+		it('should return a single requested post w correct fields', function(){
+			let post = '';
+			return chai.request(app)
+				.get('/posts')
+				.then(function(res){
+					post = res.body[0]
+					return chai.request(app)
+						.get(`/posts/${post.id}`)
+				})
+				.then(function(res){
+					expect(res.body).to.include.keys(expectedKeys);
+					expect(res.body.id).to.equal(post.id)
+				})
+		});	
 	});
 	describe('POST endpoint', function(){
-		it('should create a new blog post', function(){})
+		let preCount;
+		const newPost = createBlogPost();
+
+		it('should create a new blog post', function(){
+			return chai.request(app)
+				.post('/posts')
+				.send(newPost)
+				.then(function(res){
+					expect(res).to.have.status(201)
+					expect(res).to.be.json;
+					expect(res.body).to.be.an('object')
+					expect(res.body).to.include.keys(expectedKeys)
+					expect(res.body.id).to.not.be.null;
+					newPost.id = res.body.id;
+					expect(res.body.title).to.equal(newPost.title)
+					expect(res.body.content).to.equal(newPost.content)
+					// expect(res.body.created).to.equal(newPost.created) //changed format
+					expect(res.body.id).to.equal(newPost.id)
+					return BlogPost.findById(res.body.id)
+				})
+				.then(function(mongoPost){
+					expect(mongoPost.title).to.equal(newPost.title)
+					expect(mongoPost.content).to.equal(newPost.content)
+				})
+		})
 	});
 	describe('PUT endpoint', function(){
-		it('should update a blog post w new info', function(){})
+		it('should update a blog post w new info', function(){
+			let origPost;
+			let id;
+			const postUpdates = {  title: 'new title', 
+				author: {firstName: 'abe', lastName: 'lincoln'}
+			}
+			return BlogPost.findOne()
+				.then(function(res){
+					postUpdates.id = res.id
+					origPost = res;
+					return chai.request(app)
+						.put(`/posts/${res.id}`)
+						.send(postUpdates)
+				})
+				.then(function(res){
+					expect(res).to.have.status(204)
+					return BlogPost.findById(postUpdates.id)
+				})
+				.then(function(newPost){
+					expect(newPost.title).to.equal(postUpdates.title)
+					expect(newPost.author.firstName).to.equal(postUpdates.author.firstName)
+					expect(newPost.author.lastName).to.equal(postUpdates.author.lastName)
+					// content was NOT updated
+					expect(newPost.content).to.equal(origPost.content)
+				})
+		})
+		it('should not allow update without id in body', function(){
+			const badUpdate = {content: 'I have no id'}
+			const goodUpdate = {content: 'i have an id'}
+			return BlogPost.findOne()
+				.then(function(blogpost){
+					goodUpdate.id = blogpost.serialize().id
+					return chai.request(app)
+						.put(`/posts/${blogpost.serialize().id}`)
+						.send(badUpdate)
+				})
+				.then(function(res){
+					expect(res).to.have.status(400)
+				})
+				.catch(err=> {
+					expect(err)
+				})
+
+		})
 	});
 	describe('DELETE endpoint', function(){
-		it('should delete a given post', function(){})
+		it('should delete a given post', function(){
+			let blogpost;
+			return BlogPost
+				.findOne()
+				.then(function(_blogpost){
+					blogpost = _blogpost;
+					return chai.request(app)
+						.delete(`/posts/${blogpost.id}`);
+				})
+				.then(function(res){
+					expect(res).to.have.status(204);
+					return BlogPost.findById(blogpost.id)
+				})
+				.then(function(_blogpost){
+					expect(_blogpost).to.be.null
+				})
+		})
 	});
 
 
